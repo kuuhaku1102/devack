c992f7e9736070f49e1b27af082dab5dffaf2a98
kuuhaku1102 <j-@infintydesign.online>
2025-10-02 08:57:58 +0900

    Delete idm-membership directory

--- idm-membership/uninstall.php
+++
@@ -1,9 +0,0 @@
-<?php
-// 危険: プラグイン削除時に会員テーブル・トークンテーブルをDROPします。
-if (!defined('WP_UNINSTALL_PLUGIN')) { die; }
-global $wpdb;
-$wpdb->query("DROP TABLE IF EXISTS {$wpdb->prefix}idm_member_tokens");
-$wpdb->query("DROP TABLE IF EXISTS {$wpdb->prefix}idm_members");
-$wpdb->query("DROP TABLE IF EXISTS {$wpdb->prefix}idm_campaign_joins");
-$wpdb->query("DROP TABLE IF EXISTS {$wpdb->prefix}idm_member_tags");
-delete_option('idm_membership_version');
